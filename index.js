51011b20c856a0810efa0a41f8b366c8548c583d
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-17 00:12:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat () {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat () {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    console.log(cats.pop())
+}
+function destructivelyRemoveFirstCat() {
+    console.log (cats.shift())
+}
+function appendCat() {
+    let hews = cats.concat (["Broom"])
+    return hews
+}
+function prependCat() {
+    let newPrepend = ["Arnold", ...cats]
+    return newPrepend
+}
+function removeLastCat() {
+    let removedLastCat = cats.slice(0, -1)
+    return removedLastCat
+}
+function removeFirstCat() {
+    let removedFirstCat = cats.slice(1)
+    return removedFirstCat
+}
